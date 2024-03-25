f92ca8225f50c6628f3d3744a2558eb6626e3b2d
NotRyi0 <wpaquette455@gmail.com>
2024-03-25 04:24:29 +0000

    Why do I have an array of maps? Why not a map with multiple entries?

--- src/tools.service.ts
+++ src/tools.service.ts
@@ -12,37 +12,54 @@ class AttributeMethod extends ToolingTemplate {
    * Not sure why I did it that way, maybe not totally usefully useless
    */
   override mainTool(): void {
-    //This won't run because the list is empty so it dont go in the for loop
-    /*TODO:[please for the love of everything that is good, don't leave it like this. It's inhumane]*/
-    // this.ListOfAttributes=["Lithium"]
-    const tmpTestAttr = [];
     this.setAllAttributes();
-    const lastIndexArray = []
-    // console.log( this.countAttribute("Lithium"));
-    // for (const Attribute of this.ListOfAttributes) {
-    //   for (const AssignedAttributes of this.AllAssignedAttributes) {
-    //     console.log(`OVER if (${Attribute} === ${AssignedAttributes})`);
-    //     if (Attribute === AssignedAttributes) {
-    //       console.log(`inside if (${Attribute} === ${AssignedAttributes})`);
-    //       this.addToAttributesCount(Attribute);
-    //     } else {
-    //       this.attribKVPairArray.push(new Map([[AssignedAttributes, 0]]))
-    //       tmpTestAttr.push(AssignedAttributes);
-    //     }
-    //   }
-    // }
     for (const Attribute of this.ListOfAttributes) {
-      // console.log(this.countAttribute(Attribute))
-      this.attribKVPairArray.push(new Map([[Attribute, this.countAttribute(Attribute)]]))
-      // for (let i = 0; i < this.attribKVPairArray.length; i++) {
-      //   console.log(this.attribKVPairArray[i].get(Attribute));
-      // }
+      // console.log(this.getLastAttributeIndex(Attribute))
+      this.attribKVPairArray.push(new Map([[Attribute, this.getLastAttributeIndex(Attribute)]]))
       this.addToAttributesCount(Attribute)
     }
     console.log(this.attribKVPairArray);
+    // this.calculateRealCount(this.attribKVPairArray)
+  }
+  public AllAssignedAttributes;
+  public ListOfAttributes = new Array<string>();
+  attribKVPairArray = new Array<Map<string, number>>
 
+
+  private calculateRealCount(KVAttributesMap: Map<string, number>) {
+    const attrMap = this.attribKVPairArray;
+
+    console.log(attrMap);
+  }
+  private setAllAttributes(){
+    for (const assignedAttribute of this.AllAssignedAttributes) {
+      this.ListOfAttributes.find(attr => attr===assignedAttribute)===undefined?this.ListOfAttributes.push(assignedAttribute):true;
+    }
+    // console.log(this.ListOfAttributes)
+  }
+  private addToAttributesCount(attribute:string){
+    const attrMap = this.attribKVPairArray.find(value => value.get(attribute));
+    if (attrMap!=undefined){
+      attrMap.set(attribute, attrMap.get(attribute)!+1);
+    }
   }
 
+  private getLastAttributeIndex(attribute:string){
+    const sortedList = this.AllAssignedAttributes.sort();
+    // console.log(sortedList);
+    let count = 0;
+    for (let i = 0; i < this.AllAssignedAttributes.length; i++) {
+      if (sortedList[i]===attribute){
+        // console.log(sortedList[i]);
+        // console.log(sortedList[i+1]);
+        if (sortedList[i+1]!==attribute){
+          count = i;
+          break;
+        }
+      }
+    }
+    return count;
+  }
 
   /**
    * Get all the different attributes and assign them to an array
@@ -55,48 +72,14 @@ class AttributeMethod extends ToolingTemplate {
       return acc;
     }, []);
   }
-  public AllAssignedAttributes;
-  public ListOfAttributes = new Array<string>();
-  attribKVPairArray = new Array<Map<string, number>>
-
-  private setAllAttributes(){
-    for (const assignedAttribute of this.AllAssignedAttributes) {
-      this.ListOfAttributes.find(attr => attr===assignedAttribute)===undefined?this.ListOfAttributes.push(assignedAttribute):true;
-    }
-    console.log(this.ListOfAttributes)
-  }
-  private addToAttributesCount(attribute:string){
-    const attrMap = this.attribKVPairArray.find(value => value.get(attribute));
-    if (attrMap!=undefined){
-      attrMap.set(attribute, attrMap.get(attribute)!+1);
-    }
-  }
-
-  private countAttribute(attribute:string){
-    const sortedList = this.AllAssignedAttributes.sort();
-    // console.log(sortedList);
-    let count = 0;
-    for (let i = 0; i < this.AllAssignedAttributes.length; i++) {
-      if (sortedList[i]===attribute){
-        // console.log(sortedList[i]);
-        // console.log(sortedList[i+1]);
-        if (sortedList[i+1]!==attribute){
-
-          count = i;
-          break;
-        }
-      }
-    }
-    return count;
-  }
   constructor(name: string) {
     super();
     this.toolName = name;
     this.AllAssignedAttributes = this.GetAllAssignedStringAttributes();
   }
-
-
 }
+
+
 
 @Injectable({
   providedIn: 'root'
